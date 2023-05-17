569c514919158163768b7c3042fd49d1672b8ebe
yamataku29 <tmn.sby001@gmail.com>
2023-05-17 10:05:03 +0900

    不要なコードを削除

--- NotificationApp/ViewController.swift
+++ NotificationApp/ViewController.swift
@@ -13,14 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         requestPushAuthrization()
-        Installations.installations().installationID { (id, error) in
-            if let error = error {
-                print("‼️Error fetching id: \(error)")
-                return
-            }
-            guard let id = id else { return }
-            print("👀id: \(id)")
-        }
     }
 
 
